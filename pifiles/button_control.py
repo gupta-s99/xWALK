e2d9e19706a9694c7a537d0fcf0278d96d1235dd
jayyduran0108 <jdelator@andrew.cmu.edu>
2021-04-12 01:08:34 +0100

    added noise

--- pifiles/button_control.py
+++ pifiles/button_control.py
@@ -1,12 +1,27 @@
 from gpiozero import Button
 from picamera import PiCamera
 from time import sleep
+import pygame
+
+pygame.mixer.init()
+
+
+
+
+pygame.mixer.music.load('Ready.wav')
+pygame.mixer.music.play()
+while pygame.mixer.music.get_busy() == True:
+  continue
 
 button = Button(10)
 camera=PiCamera()
 button.wait_for_press()
 camera.start_recording('/home/pi/Desktop/video.h264')
-sleep(5)
+
+
+
+
+sleep(3)
 
 camera.stop_recording()
 camera.close()
