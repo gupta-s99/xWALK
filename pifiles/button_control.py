36d7d80af50cfa8cd45d50caa6ad7bb4eb76b4a5
jayyduran0108 <jdelator@andrew.cmu.edu>
2021-04-26 16:04:33 +0100

    changed to images

--- pifiles/button_control.py
+++ pifiles/button_control.py
@@ -1,29 +1,35 @@
 from gpiozero import Button
 from picamera import PiCamera
 from time import sleep
-import pygame
+import os, sys
+#import cv2
 
-pygame.mixer.pre_init(44100,16,2,4096)
-pygame.mixer.init()
+def get_frames():
+  cap = cv2.VideoCapture('/home/pi/Desktop/video.h264')
+  i =0
+  while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+      break
+    cv2.imwrite('test_'+str(i)+'.jpg',frame)
+    i += 1
+  cap.release()
+  cv2.destroyAllWindows()
 
-
-
-
-pygame.mixer.music.load('xWALK/pifiles/Ready.wav')
-pygame.mixer.music.play()
-while pygame.mixer.music.get_busy() == True:
-  continue
+os.system('omxplayer -o local /home/pi/xWALK/pifiles/Ready.wav');
 
 button = Button(10)
-camera=PiCamera()
-button.wait_for_press()
-camera.start_recording('/home/pi/Desktop/video.h264')
 
+while(1):  
+  button.wait_for_press()
+ 
+  camera=PiCamera()
+  sleep(2) 
+  camera.capture('/home/pi/Desktop/image.jpg')
+  #camera.stop_recording()
+  camera.close()
 
+#  get_frames()
+  os.system('omxplayer -o local /home/pi/xWALK/pifiles/Done.wav')
 
-
-sleep(3)
-
-camera.stop_recording()
-camera.close()
 print("Button was pressed")
