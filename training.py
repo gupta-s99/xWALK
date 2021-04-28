0a710021704efa37aa7d2f792578ba47c2c4487a
yasaswinid8 <54905734+yasaswinid8@users.noreply.github.com>
2021-04-28 11:27:31 -0400

    integrated hough.py into this

--- training.py
+++ training.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 from glob import glob
 import sys
 from PIL import Image
-import cv2
+import cv2 as cv
 import time 
 
 starttime = time.time()
@@ -115,7 +115,7 @@ def crop_roi_image(image_np, sel_box):
     cropped_image = image_np[int(top):int(bottom), int(left):int(right), :]
     return cropped_image
         
-test_file = "JPG/IMG_1611.jpg"
+test_file = "JPG/IMG_1594.jpg"
 
 im = Image.open(test_file)
 image_np = np.asarray(im)
@@ -130,9 +130,228 @@ except:
     cropworked = False
     
 if cropworked:
+    plt.imshow(cropped_image)
+    plt.show()
     plt.savefig(r'trafficlight.jpg')
 else: 
     print("Traffic light not found")
+
+####
+####IDENTIFYING THE COLOR OF THE TRAFFIC LIGHT 
+####
+
+def redFilter(image):
+
+    hsv = cv.cvtColor(image,cv.COLOR_BGR2HSV)
+
+    # Range for lower red
+    red_lower = np.array([0,120,70])
+    red_upper = np.array([10,255,255])
+    mask_red1 = cv.inRange(hsv, red_lower, red_upper)
+
+    # Range for upper range
+    red_lower = np.array([170,120,70])
+    red_upper = np.array([180,255,255])
+    mask_red2 = cv.inRange(hsv, red_lower, red_upper)
+
+    mask_red = mask_red1 + mask_red2
+
+    red_output = cv.bitwise_and(image, image, mask=mask_red)
+    return red_output
+
+def yellowFilter(image):
+
+    hsv = cv.cvtColor(image,cv.COLOR_BGR2HSV)
+
+    # Range for Yellow
+    yellow_lower = np.array([20, 100, 100])
+    yellow_upper = np.array([30, 255, 255])
+    mask_yellow = cv.inRange(hsv, yellow_lower, yellow_upper)
+
+    yellow_output = cv.bitwise_and(image, image, mask=mask_yellow)
+
+    return yellow_output
+
+def greenFilter(image):
+
+    hsv = cv.cvtColor(image,cv.COLOR_BGR2HSV)
+    # cv.imshow("green hsv image", hsv)
+    # cv.waitKey(0)
+
+    #Range for Green
+    green_lower = np.array([36, 0, 0])
+    green_upper = np.array([85, 255,255])
+    mask_green = cv.inRange(hsv, green_lower, green_upper)
+
+    green_output = cv.bitwise_and(image, image, mask=mask_green)
+
+    return green_output
+
+def rygFilter(image):
+
+    hsv = cv.cvtColor(image,cv.COLOR_BGR2HSV)
+
+    # Range for lower red (red is split on hsv scale)
+    red_lower = np.array([0,120,70])
+    red_upper = np.array([10,255,255])
+    mask_red1 = cv.inRange(hsv, red_lower, red_upper)
+
+    # Range for upper range
+    red_lower = np.array([170,120,70])
+    red_upper = np.array([180,255,255])
+    mask_red2 = cv.inRange(hsv, red_lower, red_upper)
+
+    # Range for Yellow
+    yellow_lower = np.array([20, 100, 100])
+    yellow_upper = np.array([30, 255, 255])
+    mask_yellow = cv.inRange(hsv, yellow_lower, yellow_upper)
+
+    #Range for Green
+    green_lower = np.array([36, 0, 0])
+    green_upper = np.array([85, 255,255])
+    mask_green = cv.inRange(hsv, green_lower, green_upper)
+
+    combined_mask = mask_red1 + mask_red2 + mask_yellow + mask_green
+
+    ryg_output = cv.bitwise_and(image, image, mask=combined_mask)
+    return ryg_output
+
+def rygwFilter(image):
+
+    hsv = cv.cvtColor(image,cv.COLOR_BGR2HSV)
+
+    # Range for lower red (red is split on hsv scale)
+    red_lower = np.array([0,120,70])
+    red_upper = np.array([10,255,255])
+    mask_red1 = cv.inRange(hsv, red_lower, red_upper)
+
+    # Range for upper range
+    red_lower = np.array([170,120,70])
+    red_upper = np.array([180,255,255])
+    mask_red2 = cv.inRange(hsv, red_lower, red_upper)
+
+    # Range for Yellow
+    yellow_lower = np.array([20, 100, 100])
+    yellow_upper = np.array([30, 255, 255])
+    mask_yellow = cv.inRange(hsv, yellow_lower, yellow_upper)
+
+    #Range for Green
+    green_lower = np.array([36, 0, 0])
+    green_upper = np.array([85, 255,255])
+    mask_green = cv.inRange(hsv, green_lower, green_upper)
+
+    #Range for White
+    lower_white = np.array([0,0,0], dtype=np.uint8)
+    upper_white = np.array([0,0,255], dtype=np.uint8)
+    mask_white = cv.inRange(hsv, lower_white, upper_white)
+
+    combined_mask = mask_red1 + mask_red2 + mask_yellow + mask_green + mask_white
+
+    rygw_output = cv.bitwise_and(image, image, mask=combined_mask)
+    return rygw_output
+
+# buggy output -> not quite an image...
+def histEqual(image):
+    # img = cv.imread('fingerptint.jpg', cv.IMREAD_UNCHANGED)
+    image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    dst = cv.equalizeHist(image)
+    cv.imshow("equalized image", dst)
+    cv.waitKey(0)
+    return dst
+
+# INTENSITY THRESHOLDING ALGORITHM HERE
+def intensityThresh(gray, threshold = 0):
+
+    ret, thresh1 = cv.threshold(gray,threshold,255,cv.THRESH_TOZERO)
+    return thresh1
+
+def main(color="red"):
+    filename = "trafficlight.jpg"
+    #src = "trafficlight.jpg"
+    src = cv.imread(cv.samples.findFile(filename), cv.IMREAD_COLOR)
+    ## [Resize Image]
+    #want the height to be H = 200 pixels
+    H = 300
+    scale = src.shape[0] / H
+    resized = cv.resize(src, (int(src.shape[1]/scale), int(src.shape[0]/scale)))
+    ## [Resize Image]
+
+    ## [Red Filter]
+    reddened = redFilter(resized)
+    ## [Red Filter]
+
+    ## [Yellow Filter]
+    yellowed = yellowFilter(resized)
+    ## [Yellow Filter]
+
+    ## [Green Filter]
+    greened = greenFilter(resized)
+    ## [Green Filter]
+
+    ## [Red, Yellow and Green Filter]
+    ryg = rygFilter(resized)
+    ## [Red, Yellow, and Green Filter]
+
+    # ## [Red, Yellow and Green + White Filter]
+    # rygw = rygwFilter(resized)
+    # ## [Red, Yellow, and Green + White Filter]
+
+    if (color == "red"):
+        filtered = reddened
+    elif (color == "yellow"):
+        filtered = yellowed
+    elif (color == "green"):
+        filtered = greened
+    else:
+        filtered = ryg
+
+
+    gray = cv.cvtColor(filtered, cv.COLOR_BGR2GRAY)
+    ## [convert_to_gray]
+
+    gray = intensityThresh(gray, threshold = 0)
+  
+    ## [reduce_noise]
+    # Reduce the noise to avoid false circle detection
+    gray = cv.medianBlur(gray, 15)
+    ## [reduce_noise]
+    
+    ## [houghcircles]
+    rows = gray.shape[0]
+    circles = cv.HoughCircles(gray, cv.HOUGH_GRADIENT, 1, rows / 8,
+                               param1=100, param2=20,
+                               minRadius=(int(H/100)), maxRadius = 50)
+    ## [houghcircles]
+
+    ## [draw]
+    if circles is not None:
+        circles = np.uint16(np.around(circles))
+        for i in circles[0, :]:
+            center = (i[0], i[1])
+            # circle center
+            cv.circle(resized, center, 1, (0, 100, 100), 3)
+            # circle outline
+            radius = i[2]
+            cv.circle(resized, center, radius, (255, 0, 255), 3)
+
+    return circles
+
+
+if __name__ == "__main__":
+    if cropworked == True: 
+        r_circles = main(color="red")
+        y_circles = main(color="yellow")
+        g_circles = main(color="green")
+        num_circles = 0
+        color_out = "none"
+        if g_circles is not None and len(g_circles) > num_circles:
+            color_out = "green"
+        if y_circles is not None and len(y_circles) >= num_circles:
+            color_out = "yellow"
+        if r_circles is not None and len(r_circles) >= num_circles:
+            color_out = "red"
+        sys.stdout.write(color_out)
+
 
 endtime = time.time()
 
